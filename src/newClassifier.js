139cdb11273492b83d41c7f6bc4d917139930a1e
syzer3@gmail.com <syzer3@gmail.com>
2017-08-29 22:34:54 +0200

    Use ml classifier

--- src/newClassifier.js
+++ src/newClassifier.js
@@ -2,11 +2,9 @@ const bayes = require('syzer-level-naive-bayes')
 const db = require('level')('./data/db')
 const nb = bayes(db)
 
-require('./loadTwitts')
+module.exports = require('./loadTwitts')
   .then(({ category1, category2 }) => Promise.all([
     category1.map(e => nb.trainAsync('category1', e)),
     category2.map(e => nb.trainAsync('category2', e))
   ]))
-  .then(() => nb.classifyAsync('code'))
-  .then(console.log)
   .catch(console.error)
---
+++ src/useClassifier.js
@@ -0,0 +1,6 @@
+const bayes = require('syzer-level-naive-bayes')
+const db = require('level')('./data/db')
+const nb = bayes(db)
+
+nb.classifyAsync('code is what i do').then(console.log)
+nb.classifyAsync('gym').then(console.log)
