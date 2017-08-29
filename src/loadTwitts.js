431cde354ee84a130d1fde6d2a59301b3ed33f76
Victor Parmar <victor.parmar@canoo.com>
2017-08-29 19:53:27 +0200

    Search for tweets

--- src/loadTwitts.js
+++ src/loadTwitts.js
@@ -7,8 +7,18 @@ const client = new Twitter({
   access_token_secret,
 })
 
-const params = { q: 'learning to code' }
+const params = { q: 'learning to code', count: 100 }
+
+const getTweetTexts = (searchResult) => {
+  const result = [];
+  const statuses = searchResult.statuses;
+  for (const tweet of statuses) {
+    result.push(tweet.text);
+  }
+  return result;
+};
 
 client.get('search/tweets', params)
+  .then(getTweetTexts)
   .then(console.log)
   .catch(console.error)
