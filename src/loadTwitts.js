28e67255e914ccda6caa5eafaf2c715c82dc8a0d
syzer3@gmail.com <syzer3@gmail.com>
2017-08-29 21:06:13 +0200

    whole pipeline, however theres a bug

--- src/loadTwitts.js
+++ src/loadTwitts.js
@@ -1,4 +1,9 @@
+const bayes = require('syzer-level-naive-bayes')
+const db = require('level')('./data/db')
+const _ = require('lodash')
+const nb = bayes(db)
 const Twitter = require('twitter')
+
 const { consumer_key, consumer_secret, access_token_key, access_token_secret } = require('./config')
 const client = new Twitter({
   consumer_key,
@@ -7,18 +12,38 @@ const client = new Twitter({
   access_token_secret
 })
 
-const params = { q: 'learning to code', count: 100 }
+const newQuery = e => ({ q: e, count: 100 })
+
+// interesting
+const category1 = [
+  'learning to code',
+  'love software'
+].map(newQuery)
+
+// not interesting
+const category2 = [
+  'going to gym',
+  'learning to take a pictures',
+  'photographhy'
+].map(newQuery)
 
 const getTweetTexts = (searchResult) => {
-  const result = [];
-  const statuses = searchResult.statuses;
+  const result = []
+  const statuses = searchResult.statuses
   for (const tweet of statuses) {
-    result.push(tweet.text);
+    result.push(tweet.text)
   }
-  return result;
-};
+  return result
+}
 
-client.get('search/tweets', params)
-  .then(getTweetTexts)
-  .then(console.log)
+const newCategory = (category = category1, categoryName = 'category1') => Promise.all(
+  category.map(params => client.get('search/tweets', params).then(getTweetTexts))
+).then(responses => responses.map(e => _.uniq(e)))
+  .then(responses => responses.map(e => nb.trainAsync(categoryName, e)))
   .catch(console.error)
+
+newCategory()
+  .then(() => newCategory(category2, 'category2'))
+  .then(() => nb.classifyAsync('gym')
+    .then(console.log))
+
