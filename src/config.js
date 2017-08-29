1cc8b1ac0d6f0d9604a012b363e94e6973c13798
syzer3@gmail.com <syzer3@gmail.com>
2017-08-29 19:46:27 +0200

    check if all keys present

--- src/config.js
+++ src/config.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 
-module.exports = {
+const config =  {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token_key: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-
 }
+
+const allKeysPresent = Object.values(config).filter(e => Boolean(e)).length === Object.keys(config).length
+if (!allKeysPresent) {
+  throw Error('Not all twitter data initialized')
+}
+
+module.exports = config
