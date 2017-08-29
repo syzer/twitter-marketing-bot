2ad10a474b0b71209fb103734bb985da20737cf0
syzer3@gmail.com <syzer3@gmail.com>
2017-08-29 19:31:24 +0200

    promisified twitter

--- src/loadTwitts.js
+++ src/loadTwitts.js
@@ -1,4 +1,5 @@
 const Twitter = require('twitter')
+const q = require('bluebird')
 const { consumer_key, consumer_secret, access_token_key, access_token_secret } = require('./config')
 const client = new Twitter({
   consumer_key,
@@ -8,9 +9,12 @@ const client = new Twitter({
 })
 
 const params = { screen_name: 'nodejs' }
-client.get('statuses/user_timeline', params, (error, tweets, response) => {
-  if (!error) {
-    console.log(tweets)
-  }
-  console.error(error)
+
+const promissedGet = new Promise((resolve, reject) => {
+
 })
+q.promisifyAll(client)
+
+client.getAsync('statuses/user_timeline', params)
+  .then(console.warn)
+  .catch(console.error)
