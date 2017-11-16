10a39fa2eaf001faf37c57ba0a5bfaa9d20a6a17
syzer3@gmail.com <syzer3@gmail.com>
2017-11-16 12:21:20 +0100

    more test for parsers

--- test/twitter/parser.test.js
+++ test/twitter/parser.test.js
@@ -3,3 +3,7 @@ const {tokenizeAndStem} = require('../../src/twitter/parser')
 test('stemming and tokenizing', () =>
   expect(tokenizeAndStem(`McAfee's own anti-hacking service exposed users to banking malware`))
     .toBe('mcafe anti hack servic expos user bank malwar'))
+
+test('stemming and tokenizing', () =>
+  expect(tokenizeAndStem(`If App Center is out of beta, when will @hockeyapp go away?`))
+    .toBe('app center beta will go awai'))
